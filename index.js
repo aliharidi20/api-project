28865acad685bcab0df7d64af78049c58119b702
aliharidi20 <ali.h.haridi>
2025-06-25 16:11:06 +0300

    del line

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.get('/', async (req, res) => {
   const result = await pool.query('SELECT * FROM consumer_complaints where complaint_id = 482443');
   const data = res.json(result.rows.length);
   console.log(result.rows[0].company)
-  result.rows.length
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
