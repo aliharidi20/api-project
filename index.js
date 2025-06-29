013cde9a3c870b6224f77f2537528ff1509550e9
aliharidi20 <ali.h.haridi>
2025-06-29 11:38:33 +0300

    update2

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const { Pool } = require('pg');
+const cors = require('cors')
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
